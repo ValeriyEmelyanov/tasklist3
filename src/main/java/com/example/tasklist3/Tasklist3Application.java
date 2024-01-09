dcf391be5e53241fea3691d8961cabb0675409bb
Valeriy Emelyanov <emelva@rambler.ru>
2024-01-09 21:32:08 +0300

    start

---
+++ src/main/java/com/example/tasklist3/Tasklist3Application.java
@@ -0,0 +1,13 @@
+package com.example.tasklist3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tasklist3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tasklist3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasklist3/Tasklist3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasklist3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tasklist3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
