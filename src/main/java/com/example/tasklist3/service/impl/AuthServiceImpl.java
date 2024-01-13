5a19e8d0bb1e0061a2fa22f1adb0a8a454a3f330
Valeriy Emelyanov <emelva@rambler.ru>
2024-01-13 21:36:37 +0300

    auth controller and service

--- src/main/java/com/example/tasklist3/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/tasklist3/service/impl/AuthServiceImpl.java
@@ -1,21 +1,39 @@
 package com.example.tasklist3.service.impl;
 
+import com.example.tasklist3.domain.user.User;
 import com.example.tasklist3.service.AuthService;
+import com.example.tasklist3.service.UserService;
 import com.example.tasklist3.web.dto.auth.JwtRequest;
 import com.example.tasklist3.web.dto.auth.JwtResponse;
+import com.example.tasklist3.web.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public JwtResponse login(JwtRequest loginRequest) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        User user = userService.getByUsername(loginRequest.getUsername());
+        JwtResponse jwtResponse = new JwtResponse();
+        jwtResponse.setId(user.getId());
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(user.getId(), user.getUsername(), user.getRoles()));
+        jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(user.getId(), user.getUsername()));
+        return jwtResponse;
     }
 
     @Override
     public JwtResponse refresh(String refreshToken) {
-        return null;
+        return jwtTokenProvider.refreshUserTokens(refreshToken);
     }
 
 }
---
+++ src/main/java/com/example/tasklist3/web/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.example.tasklist3.web.controller;
+
+import com.example.tasklist3.domain.user.User;
+import com.example.tasklist3.service.AuthService;
+import com.example.tasklist3.service.UserService;
+import com.example.tasklist3.web.dto.auth.JwtRequest;
+import com.example.tasklist3.web.dto.auth.JwtResponse;
+import com.example.tasklist3.web.dto.user.UserDto;
+import com.example.tasklist3.web.dto.validation.OnCreate;
+import com.example.tasklist3.web.mappers.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/va/auth")
+@RequiredArgsConstructor
+@Validated
+public class AuthController {
+
+    private final AuthService authService;
+    private final UserService userService;
+
+    private final UserMapper userMapper;
+
+    @PostMapping("/login")
+    public JwtResponse login(@Validated @RequestBody final JwtRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    @PostMapping("/register")
+    public UserDto register(@Validated(OnCreate.class) @RequestBody final UserDto userDto) {
+        User user = userMapper.toEntity(userDto);
+        User createdUser = userService.create(user);
+        return userMapper.toDto(createdUser);
+    }
+
+    @PostMapping("/refresh")
+    public JwtResponse refresh(@RequestBody final String refreshToken) {
+        return authService.refresh(refreshToken);
+    }
+
+}
