721edc2efffa34f1f875f4b5df9cfd7ead0a258d
Valeriy Emelyanov <emelva@rambler.ru>
2024-01-14 21:55:14 +0300

    task repository implementation with jdbc

--- src/main/java/com/example/tasklist3/repository/impl/TaskRepositoryImpl.java
+++ src/main/java/com/example/tasklist3/repository/impl/TaskRepositoryImpl.java
@@ -1,41 +1,162 @@
 package com.example.tasklist3.repository.impl;
 
+import com.example.tasklist3.domain.exception.ResourceNotFoundException;
 import com.example.tasklist3.domain.task.Task;
+import com.example.tasklist3.repository.DataSourceConfig;
 import com.example.tasklist3.repository.TaskRepository;
+import com.example.tasklist3.repository.mappers.TaskRowMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class TaskRepositoryImpl implements TaskRepository {
+
+    private final DataSourceConfig dataSourceConfig;
+
+    private final String FIND_BY_ID = """
+            SELECT t.id as task_id,
+                   t.title as task_title,
+                   t.description as task_description,
+                   t.expiration_date as task_expiration_date,
+                   t.status as task_status
+            FROM tasks t
+            WHERE id = ?""";
+
+    private final String FIND_ALL_BY_USER_ID = """
+            SELECT t.id as task_id,
+                   t.title as task_title,
+                   t.description as task_description,
+                   t.expiration_date as task_expiration_date,
+                   t.status as task_status
+            FROM tasks t
+                JOIN users_tasks ut on t.id = ut.task_id
+            WHERE ut.user_id = ?;""";
+
+    private final String ASSIGN = """
+            INSERT INTO users_tasks (task_id, user_id)
+            VALUES (?, ?);""";
+
+    private final String UPDATE = """
+            UPDATE tasks
+            SET title = ?,
+                description = ?,
+                expiration_date = ?,
+                status = ?
+            WHERE id = ?""";
+
+    private final String CREATE = """
+            INSERT INTO tasks (title, description, expiration_date, status)
+            VALUES (?, ?, ?, ?)""";
+
+    private final String DELETE = """
+            DELETE FROM tasks
+            WHERE id = ?""";
+
     @Override
     public Optional<Task> findById(Long id) {
-        return Optional.empty();
+        try (Connection connection = dataSourceConfig.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(FIND_BY_ID);
+            statement.setLong(1, id);
+            try (ResultSet rs = statement.executeQuery()) {
+                return Optional.ofNullable(TaskRowMapper.mapRow(rs));
+            }
+        } catch (SQLException e) {
+            throw new ResourceNotFoundException("Error while finding task by id.");
+        }
     }
 
     @Override
-    public List<Task> findAllByUserId(Long id) {
-        return null;
+    public List<Task> findAllByUserId(Long userId) {
+        try (Connection connection = dataSourceConfig.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(FIND_ALL_BY_USER_ID);
+            statement.setLong(1, userId);
+            try (ResultSet rs = statement.executeQuery()) {
+                return TaskRowMapper.mapRows(rs);
+            }
+        } catch (SQLException e) {
+            throw new ResourceNotFoundException("Error while finding all tasks by user id.");
+        }
     }
 
     @Override
     public void assignToUserById(Long taskId, Long userId) {
-
+        try (Connection connection = dataSourceConfig.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(ASSIGN);
+            statement.setLong(1, taskId);
+            statement.setLong(2, userId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ResourceNotFoundException("Error while assigning to user.");
+        }
     }
 
     @Override
     public void update(Task task) {
-
+        try (Connection connection = dataSourceConfig.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(UPDATE);
+            statement.setString(1, task.getTitle());
+            if (task.getDescription() == null) {
+                statement.setNull(2, Types.VARCHAR);
+            } else {
+                statement.setString(2, task.getDescription());
+            }
+            if (task.getExpirationDate() == null) {
+                statement.setNull(3, Types.TIMESTAMP);
+            } else {
+                statement.setTimestamp(3, Timestamp.valueOf(task.getExpirationDate()));
+            }
+            statement.setString(4, task.getStatus().name());
+            statement.setLong(5, task.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ResourceNotFoundException("Error while updating task.");
+        }
     }
 
     @Override
     public void create(Task task) {
-
+        try (Connection connection = dataSourceConfig.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, task.getTitle());
+            if (task.getDescription() == null) {
+                statement.setNull(2, Types.VARCHAR);
+            } else {
+                statement.setString(2, task.getDescription());
+            }
+            if (task.getExpirationDate() == null) {
+                statement.setNull(3, Types.TIMESTAMP);
+            } else {
+                statement.setTimestamp(3, Timestamp.valueOf(task.getExpirationDate()));
+            }
+            statement.setString(4, task.getStatus().name());
+            statement.executeUpdate();
+            try (ResultSet rs = statement.getGeneratedKeys()) {
+                rs.next();
+                task.setId(rs.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new ResourceNotFoundException("Error while updating task.");
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        try (Connection connection = dataSourceConfig.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(DELETE);
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ResourceNotFoundException("Error while deleting task.");
+        }
     }
 }
---
+++ src/main/java/com/example/tasklist3/repository/mappers/TaskRowMapper.java
@@ -0,0 +1,44 @@
+package com.example.tasklist3.repository.mappers;
+
+import com.example.tasklist3.domain.task.Status;
+import com.example.tasklist3.domain.task.Task;
+import lombok.SneakyThrows;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRowMapper {
+
+    @SneakyThrows
+    public static Task mapRow(ResultSet rs) {
+        if (rs.next()) {
+            return createTaskFromRow(rs);
+        }
+        return null;
+    }
+
+    private static Task createTaskFromRow(ResultSet rs) throws SQLException {
+        Task task = new Task();
+        task.setId(rs.getLong("task_id"));
+        task.setTitle(rs.getString("task_title"));
+        task.setDescription(rs.getString("task_description"));
+        task.setStatus(Status.valueOf(rs.getString("task_status")));
+        Timestamp expirationDate = rs.getTimestamp("task_expiration_date");
+        if (expirationDate != null) {
+            task.setExpirationDate(expirationDate.toLocalDateTime());
+        }
+        return task;
+    }
+
+    @SneakyThrows
+    public static List<Task> mapRows(ResultSet rs) {
+        List<Task> tasks = new ArrayList<>();
+        while (rs.next()) {
+            tasks.add(createTaskFromRow(rs));
+        }
+        return tasks;
+    }
+}
