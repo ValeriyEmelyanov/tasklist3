dd1eef9a2cb0ea107fcfe02bfeea703b008b99a4
Valeriy Emelyanov <emelva@rambler.ru>
2024-02-25 22:01:10 +0300

    refactoring

--- src/main/java/com/example/tasklist3/repository/TaskRepository.java
+++ src/main/java/com/example/tasklist3/repository/TaskRepository.java
@@ -2,6 +2,7 @@ package com.example.tasklist3.repository;
 
 import com.example.tasklist3.domain.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -16,4 +17,17 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             """, nativeQuery = true)
     List<Task> findAllByUserId(@Param("userId") Long userId);
 
+    @Modifying
+    @Query(value = """
+            INSERT INTO users_tasks (user_id, task_id)
+                        VALUES (:userId, :taskId)
+            """, nativeQuery = true)
+    void assignTask(@Param("userId") Long userId, @Param("taskId") Long id);
+
+    @Modifying
+    @Query(value = """
+            INSERT INTO tasks_images (task_id, image)
+                        VALUES (:id, :fileName)
+            """, nativeQuery = true)
+    void addImage(@Param("id") Long id, @Param("filename") String filename);
 }
--- src/main/java/com/example/tasklist3/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/tasklist3/service/impl/TaskServiceImpl.java
@@ -23,7 +23,6 @@ import java.util.List;
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
-    private final UserService userService;
     private final ImageService imageService;
 
     @Override
@@ -59,11 +58,11 @@ public class TaskServiceImpl implements TaskService {
             key = "#task.id"
     )
     public Task create(final Task task, final Long userId) {
-        task.setStatus(Status.TODO);
+        if (task.getStatus() == null) {
+            task.setStatus(Status.TODO);
+        }
         taskRepository.save(task);
-        User user = userService.getById(userId);
-        user.getTasks().add(task);
-        userService.update(user);
+        taskRepository.assignTask(userId, task.getId());
         return task;
     }
 
@@ -78,9 +77,7 @@ public class TaskServiceImpl implements TaskService {
     @Transactional
     @CacheEvict(value = "TaskService::getById", key = "#id")
     public void uploadImage(final Long id, final TaskImage image) {
-        Task task = getById(id);
         String filename = imageService.upload(image);
-        task.getImages().add(filename);
-        taskRepository.save(task);
+        taskRepository.addImage(id, filename);
     }
 }
